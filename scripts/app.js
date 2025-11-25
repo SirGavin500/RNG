3d850af9ca188d31aa3a1918b87ab7cf127a3827
SirGavin500 <gaqtristan@gmail.com>
2025-11-25 13:05:34 -0800

    Changes

--- scripts/app.js
+++ scripts/app.js
@@ -6,31 +6,27 @@ let personalEmail = document.getElementById("personalEmail");
 let randomPeerBtn = document.getElementById("randomPeerBtn");
 let historyList = document.getElementById("historyList");
 
-let history = [];
+let history = []; 
+let currentStudent = null; 
 
 function getData() {
     return fetch("../data/data.json")
         .then(res => res.json())
-        .then(data => data.students);
+        .then(data => data.students)
+        .catch(err => console.error("Error fetching data:", err));
 }
 
 function randomizeData(students) {
     let randomIndex = Math.floor(Math.random() * students.length);
     return students[randomIndex];
 }
-
-function updateHistory(randomStudent) {
-    history.unshift(randomStudent);
-
-    if (history.length > 5) {
-        history.pop();
-    }
-
+// made it so both emails are displayed in history
+function updateHistory() {
     historyList.innerHTML = "";
 
     history.forEach(student => {
         let li = document.createElement("li");
-        li.innerText = `${student.firstName} ${student.lastName} - ${student.role} - ${student.email}`;
+        li.innerText = `${student.firstName} ${student.lastName} - ${student.role} - ${student.email} - ${student.personalEmail}`;
         historyList.appendChild(li);
     });
 }
@@ -39,13 +35,28 @@ randomPeerBtn.addEventListener("click", () => {
     getData().then((students) => {
         let randomStudent = randomizeData(students);
 
+        // fixed issue with history showing current peer in the previous 5.
+        if (currentStudent) {
+            history.unshift(currentStudent);
+            if (history.length > 5) {
+                history.pop(); 
+            }
+        }
+
         firstName.innerText = randomStudent.firstName;
         lastName.innerText = randomStudent.lastName;
         house.innerText = randomStudent.role;
-
         email.innerText = randomStudent.email;
         personalEmail.innerText = randomStudent.personalEmail;
 
-        updateHistory(randomStudent);
+        currentStudent = {
+            firstName: randomStudent.firstName,
+            lastName: randomStudent.lastName,
+            role: randomStudent.role,
+            email: randomStudent.email,
+            personalEmail: randomStudent.personalEmail
+        };
+
+        updateHistory();
     });
 });
